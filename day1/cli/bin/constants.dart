5774ccb963638e7f31b839a8eeb8801ab8bb59dc
FelipeAlafy <69154350+FelipeAlafy@users.noreply.github.com>
2021-08-05 08:12:54 -0300

    Day 1
    main
    print
    variables
    constants

---
+++ day1/cli/bin/constants.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  //Constants
+  const hello = 'Hello, World! constant declaration 1';
+  final hello2 = ('Hello, World! constant declaration 2');
+
+  print(hello);
+  print(hello2);
+}
