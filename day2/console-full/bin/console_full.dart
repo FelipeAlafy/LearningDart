52b3bd4d5166eafcf363463283359584eb85e28d
FelipeAlafy <69154350+FelipeAlafy@users.noreply.github.com>
2021-08-05 14:07:09 -0300

    Day 2 conditional structures part one.

---
+++ day2/console-full/bin/console_full.dart
@@ -0,0 +1,12 @@
+void main(List<String> arguments) {
+  var conditionalValue = 2;
+  if (conditionalValue == 1) {
+    print('The variable value is one!');
+  }
+  if (conditionalValue >= 1) {
+    print('The variable value is more than one!');
+  }
+  if (conditionalValue == 2) {
+    print('The variable value is two!');
+  }
+}
---
+++ day2/console-full/bin/else.dart
@@ -0,0 +1,8 @@
+main(List<String> args) {
+  var conditionalValue = 2;
+  if (conditionalValue == 1) {
+    print('The conditional value is one');
+  } else {
+    print('The conditional value is not one');
+  }
+}
