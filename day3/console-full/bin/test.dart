6a2e19cdc6f11316a41fb503534d5425ea7696e8
FelipeAlafy <69154350+FelipeAlafy@users.noreply.github.com>
2021-08-06 09:45:17 -0300

    Update test.dart

--- day3/console-full/bin/test.dart
+++ day3/console-full/bin/test.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main(List<String> args) {
   print('Enter with a numeric value: ');
   var c = stdin.readLineSync();
-  var a = c as int;
+  var a = int.parse(c!);
   while (a >= 0) {
     print('$a');
     a--;
