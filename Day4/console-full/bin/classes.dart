81bef4a7c2ed254599a9eb351abe69ea541d26dc
FelipeAlafy <69154350+FelipeAlafy@users.noreply.github.com>
2021-08-07 14:05:41 -0300

    Class and constructors

---
+++ Day4/console-full/bin/classes.dart
@@ -0,0 +1,46 @@
+class HelloWorld {
+  int defaultClassValue = 4;
+  int getDoubleOfDefaultValue() {
+    return (defaultClassValue * 2);
+  }
+}
+
+class Hello_Two {
+  int HelloLevel = 1;
+
+  Hello_Two(int helloLevel) {
+    HelloLevel = helloLevel; 
+  }
+
+  void printHello() {
+    switch (HelloLevel) {
+      case 0:
+        print('...');
+        break;
+      
+      case 1:
+        print('Hello');
+        break;
+      
+      case 2:
+        print('Hello World');
+        break;
+      
+      case 3:
+        print('Hello, World!');
+        break;
+      
+      default:
+        print('Hello');
+    }
+  }
+}
+
+void main(List<String> args) {
+  var hello = HelloWorld();
+  var control = Hello_Two(0);
+  print('Default class value: ${hello.defaultClassValue}');
+  print('Default class value: ${hello.getDoubleOfDefaultValue()}');
+  print('Hello_Two:');
+  control.printHello();
+}
