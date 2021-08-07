3a3c528c9f6ecc15aa6ecc2a19a0f46f1c8c94d4
FelipeAlafy <69154350+FelipeAlafy@users.noreply.github.com>
2021-08-07 13:29:47 -0300

    Functions

---
+++ Day4/console-full/bin/functionReturnable.dart
@@ -0,0 +1,19 @@
+//Returnables functions
+
+void main(List<String> args) {
+  print(aValue());
+  print(hello());
+  print(doit());
+}
+
+int aValue() {
+  return 13;
+}
+
+String hello() {
+  return 'Hello, World!';
+}
+
+bool doit() {
+  return true;
+}
---
+++ Day4/console-full/bin/functions.dart
@@ -0,0 +1,8 @@
+//Non returnable function
+void main(List<String> arguments) {
+  FunctionHello();
+}
+
+void FunctionHello() {
+  print('Hello, World! from FunctionHello');
+}
